3f5e2a8821b189235b4a225d049e7ce7cefb36a1
creatorlxd <creatorlxd@outlook.com>
2016-05-08 18:07:25 +0800

    update objectprint

--- source/tools.cpp
+++ source/tools.cpp
@@ -2,12 +2,13 @@
 #include "tools.h"
 
 
-void object::init(LPDIRECT3DDEVICE9 g_pd3dDevice, int VertexSize, int IndexSize, CUSTOMVERTEX Vertices[], WORD Indices[])
+void object::init(LPDIRECT3DDEVICE9 g_pd3dDevice, int VertexSize, int IndexSize, CUSTOMVERTEX Vertices[], WORD Indices[],int PrimitiveCount)
 {
 	
 	g_VertexSize = VertexSize;
 	g_IndexSize = IndexSize;
-	g_pd3dDevice->CreateVertexBuffer(g_VertexSize*sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVertexbuffer, NULL);
+	g_PrimitiveCount = PrimitiveCount;
+	g_pd3dDevice->CreateVertexBuffer(g_VertexSize*sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVertexBuffer, NULL);
 	g_pd3dDevice->CreateIndexBuffer(g_IndexSize*sizeof(WORD), 0, D3DFMT_INDEX16, D3DPOOL_DEFAULT, &g_pIndexBuffer, NULL);
 	WriteInVertexBuffer(Vertices);
 	WriteInIndexBuffer(Indices);
@@ -15,7 +16,7 @@ void object::init(LPDIRECT3DDEVICE9 g_pd3dDevice, int VertexSize, int IndexSize,
 
 object::~object()
 {
-	SAFE_RELEASE(g_pVertexbuffer);
+	SAFE_RELEASE(g_pVertexBuffer);
 	SAFE_RELEASE(g_pIndexBuffer);
 }
 
@@ -23,7 +24,8 @@ object::object()
 {
 	g_VertexSize = 0;
 	g_IndexSize = 0;
-	g_pVertexbuffer = NULL;
+	g_PrimitiveCount = 0;
+	g_pVertexBuffer = NULL;
 	g_pIndexBuffer = NULL;
 	pVertices = NULL;
 	pIndices = NULL;
@@ -31,7 +33,7 @@ object::object()
 
 void object::WriteInVertexBuffer(CUSTOMVERTEX Vertices[])
 {
-	g_pVertexbuffer->Lock(0, sizeof(Vertices), (void**)&pVertices, 0);
+	g_pVertexBuffer->Lock(0, sizeof(Vertices), (void**)&pVertices, 0);
 	memcpy(pVertices, Vertices, sizeof(Vertices));
 	g_pIndexBuffer->Unlock();
 }
@@ -41,4 +43,12 @@ void object::WriteInIndexBuffer(WORD Indices[])
 	g_pIndexBuffer->Lock(0, sizeof(Indices), (void**)&pIndices, 0);
 	memcpy(pIndices, Indices, sizeof(Indices));
 	g_pIndexBuffer->Unlock();
+}
+
+void object::ObjectPrint(LPDIRECT3DDEVICE9 g_pd3dDevice)
+{
+	g_pd3dDevice->SetStreamSource(0, g_pVertexBuffer, 0, sizeof(CUSTOMVERTEX));
+	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+	g_pd3dDevice->SetIndices(g_pIndexBuffer);
+	g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, g_VertexSize, 0,(UINT)g_PrimitiveCount);
 }
--- source/tools.h
+++ source/tools.h
@@ -21,17 +21,18 @@ struct CUSTOMVERTEX //顶点结构体
 class object    //DX3D物体对象
 {
 public:
-	void init(LPDIRECT3DDEVICE9 g_pd3dDevice,int VertexSize,int IndexSize,CUSTOMVERTEX Vertices[],WORD Indices[]);	//创建顶点缓存和索引缓存,VertexSize为顶点数,IndexSize为索引数（索引的多少）
+	void init(LPDIRECT3DDEVICE9 g_pd3dDevice,int VertexSize,int IndexSize,CUSTOMVERTEX Vertices[],WORD Indices[],int PrimitiveCount);	//创建顶点缓存和索引缓存,VertexSize为顶点数,IndexSize为索引数（索引的多少）
 	~object();																//释放顶点缓存和索引缓存
 	object();																//初始化
 	void WriteInVertexBuffer(CUSTOMVERTEX Vertices[]);					//写入顶点缓存内容
 	void WriteInIndexBuffer(WORD Indices[]);							//写入索引缓存内容
-
+	void ObjectPrint(LPDIRECT3DDEVICE9 g_pd3dDevice);					//图形的绘制
 private:
-	LPDIRECT3DVERTEXBUFFER9 g_pVertexbuffer;	//顶点缓存
+	LPDIRECT3DVERTEXBUFFER9 g_pVertexBuffer;	//顶点缓存
 	LPDIRECT3DINDEXBUFFER9 g_pIndexBuffer;		//索引缓存
 	int g_VertexSize;							//顶点数
 	int g_IndexSize;							//索引数
-	VOID* pVertices;							//指向顶点缓存内部数据数组的指针，其值恒为0，故只是临时的。
-	WORD* pIndices;								//指向索引缓存内部数据数组的指针，其值恒为0，故只是临时的。
+	VOID* pVertices;							//指向顶点缓存内部数据数组的指针
+	WORD* pIndices;								//指向索引缓存内部数据数组的指针
+	int g_PrimitiveCount;						//图元的数量
 };
