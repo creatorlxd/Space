52762d91685002d6dd591c57c9a1400b68839513
creatorlxd <creatorlxd@outlook.com>
2016-05-27 21:05:14 +0800

    a big update

--- source/object.cpp
+++ source/object.cpp
@@ -2,12 +2,12 @@
 #include "object.h"
 
 
-void object::init(LPDIRECT3DDEVICE9 g_pd3dDevice, int VertexSize, int IndexSize, CUSTOMVERTEX Vertices[], WORD Indices[],int PrimitiveCount)
+void object::init(LPDIRECT3DDEVICE9 g_pd3dDevice, int VertexSize, int IndexSize, CUSTOMVERTEX Vertices[], WORD Indices[])
 {
 	
 	m_VertexSize = VertexSize;
 	m_IndexSize = IndexSize;
-	m_PrimitiveCount = PrimitiveCount;
+	m_PrimitiveCount = m_IndexSize/3;
 	g_pd3dDevice->CreateVertexBuffer(m_VertexSize*sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &m_pVertexBuffer, NULL);
 	g_pd3dDevice->CreateIndexBuffer(m_IndexSize*sizeof(WORD), 0, D3DFMT_INDEX16, D3DPOOL_DEFAULT, &m_pIndexBuffer, NULL);
 	WriteInVertexBuffer(Vertices);
@@ -27,21 +27,21 @@ object::object()
 	m_PrimitiveCount = 0;
 	m_pVertexBuffer = NULL;
 	m_pIndexBuffer = NULL;
-	mVertices = NULL;
-	mIndices = NULL;
+	mpVertices = NULL;
+	mpIndices = NULL;
 }
 
 void object::WriteInVertexBuffer(CUSTOMVERTEX Vertices[])
 {
-	m_pVertexBuffer->Lock(0, 0, (void**)&mVertices, 0);
-	memcpy(mVertices, Vertices, m_VertexSize*sizeof(CUSTOMVERTEX));
+	m_pVertexBuffer->Lock(0, 0, (void**)&mpVertices, 0);
+	memcpy(mpVertices, Vertices, m_VertexSize*sizeof(CUSTOMVERTEX));
 	m_pVertexBuffer->Unlock();
 }
 
 void object::WriteInIndexBuffer(WORD Indices[])
 {
-	m_pIndexBuffer->Lock(0, 0, (void**)&mIndices, 0);
-	memcpy(mIndices, Indices, m_IndexSize*sizeof(WORD));
+	m_pIndexBuffer->Lock(0, 0, (void**)&mpIndices, 0);
+	memcpy(mpIndices, Indices, m_IndexSize*sizeof(WORD));
 	m_pIndexBuffer->Unlock();
 }
 
--- source/object.h
+++ source/object.h
@@ -21,7 +21,7 @@ struct CUSTOMVERTEX //顶点结构体
 class object    //DX3D物体对象
 {
 public:
-	void init(LPDIRECT3DDEVICE9 g_pd3dDevice,int VertexSize,int IndexSize,CUSTOMVERTEX Vertices[],WORD Indices[],int PrimitiveCount);	//创建顶点缓存和索引缓存,VertexSize为顶点数,IndexSize为索引数（索引的多少）
+	void init(LPDIRECT3DDEVICE9 g_pd3dDevice,int VertexSize,int IndexSize,CUSTOMVERTEX Vertices[],WORD Indices[]);	//创建顶点缓存和索引缓存,VertexSize为顶点数,IndexSize为索引数（索引的多少）
 	~object();																//释放顶点缓存和索引缓存
 	object();																//初始化
 	void WriteInVertexBuffer(CUSTOMVERTEX Vertices[]);					//写入顶点缓存内容
@@ -32,7 +32,7 @@ private:
 	LPDIRECT3DINDEXBUFFER9 m_pIndexBuffer;		//索引缓存
 	int m_VertexSize;							//顶点数
 	int m_IndexSize;							//索引数
-	VOID* mVertices;							//指向顶点缓存内部数据数组的指针
-	WORD* mIndices;								//指向索引缓存内部数据数组的指针
+	VOID* mpVertices;							//指向顶点缓存内部数据数组的指针
+	WORD* mpIndices;								//指向索引缓存内部数据数组的指针
 	int m_PrimitiveCount;						//图元的数量
 };
