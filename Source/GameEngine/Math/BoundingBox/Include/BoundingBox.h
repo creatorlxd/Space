498a557988071e47418b7eea32235dd23f9ec801
creatorlxd <creatorlxd@outlook.com>
2017-10-24 23:16:57 +0800

    feat: add some code about aabb

--- Source/GameEngine/Math/BoundingBox/Include/BoundingBox.h
+++ Source/GameEngine/Math/BoundingBox/Include/BoundingBox.h
@@ -3,5 +3,13 @@
 
 namespace SpaceGameEngine
 {
+	struct AxisAlignedBoundingBox		//AABB°üÎ§ºÐ
+	{
+		XMFLOAT3 m_MinPosition;
+		XMFLOAT3 m_MaxPosition;
+	};
 
+	typedef AxisAlignedBoundingBox AABB;
+
+	bool IfIntersect(const AxisAlignedBoundingBox& aabb1, const AxisAlignedBoundingBox& aabb2);
 }
--- Source/GameEngine/Math/BoundingBox/Source/BoundingBox.cpp
+++ Source/GameEngine/Math/BoundingBox/Source/BoundingBox.cpp
@@ -1,2 +1,13 @@
 #include "stdafx.h"
 #include "../Include/BoundingBox.h" 
+
+bool SpaceGameEngine::IfIntersect(const AxisAlignedBoundingBox & aabb1, const AxisAlignedBoundingBox & aabb2)
+{
+	if (aabb1.m_MinPosition.x > aabb2.m_MaxPosition.x || aabb1.m_MaxPosition.x < aabb2.m_MinPosition.x)
+		return false;
+	if (aabb1.m_MinPosition.y > aabb2.m_MaxPosition.y || aabb1.m_MaxPosition.y < aabb2.m_MinPosition.y)
+		return false;
+	if (aabb1.m_MinPosition.z > aabb2.m_MaxPosition.z || aabb1.m_MaxPosition.z < aabb2.m_MinPosition.z)
+		return false;
+	return true;
+}
