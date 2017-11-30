ad2359a6f209fe789a8f2e6a39d48ad939b8ecdc
creatorlxd <creatorlxd@outlook.com>
2017-11-30 11:44:16 +0800

    fix light

--- Source/GameEngine/Graphics/Light/Include/Light.h
+++ Source/GameEngine/Graphics/Light/Include/Light.h
@@ -18,6 +18,13 @@ limitations under the License.
 
 namespace SpaceGameEngine
 {
+	enum LightType :uint32_t
+	{
+		DirectionLight = 1,
+		PointLight = 2,
+		SpotLight = 4
+	};
+
 	/*
 	基本的光源信息，涵盖了基本的三种光源（平行光、点光源、聚光灯）的信息
 	*/
@@ -29,7 +36,13 @@ namespace SpaceGameEngine
 		XMFLOAT4 m_Diffuse;			//漫反射光
 		XMFLOAT4 m_Specular;		//镜面反射光
 
-		//方向光所特有的
-		XMFLOAT3 Direction;
+		XMFLOAT3 m_Position;
+		float m_Range;
+
+		XMFLOAT3 m_Direction;		//方向光和聚光灯所特有的光的方向
+		float m_SpotLightOption;	//聚光灯的参数
+
+		XMFLOAT3 m_LightOption;		//光源的基本的三个参数:a0,a1,a2-->a0+a1*d+a2*d^2
+		LightType m_Type;			//光源的类型
 	};
 }
