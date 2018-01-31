183e422babe2c3b4dcf5116f5e4d37ccfe6550e6
creatorlxd <creatorlxd@outlook.com>
2018-01-31 14:40:19 +0800

    fix a bug in string converter

--- Source/GameEngine/Common/Utility/Source/StringConverter.cpp
+++ Source/GameEngine/Common/Utility/Source/StringConverter.cpp
@@ -18,19 +18,33 @@ limitations under the License.
 
 std::wstring SpaceGameEngine::StringToWString(const std::string& str)
 {
-	std::wstring wstr;
+	/*std::wstring wstr;
 	size_t size;
 	wstr.resize(str.length());
 	mbstowcs_s(&size, &wstr[0], wstr.size() + 1, str.c_str(), str.size());
+	return wstr;*/
+
+	std::wstring wstr;
+	size_t size = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
+	wstr.resize(size);
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, &wstr[0], size);
+	wstr.erase(wstr.end() - 1);
 	return wstr;
 }
 
 std::string SpaceGameEngine::WStringToString(const std::wstring & wstr)
 {
-	std::string str;
+	/*std::string str;
 	size_t size;
 	str.resize(wstr.length());
 	wcstombs_s(&size, &str[0], str.size() + 1, wstr.c_str(), wstr.size());
+	return str;*/
+
+	std::string str;
+	size_t size = WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
+	str.resize(size);
+	WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), -1, &str[0], size, NULL, NULL);
+	str.erase(str.end() - 1);
 	return str;
 }
 
