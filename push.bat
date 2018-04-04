c02e7af4940312318f4e09f352a588338ba5b570
creatorlxd <creatorlxd@outlook.com>
2018-04-04 23:13:17 +0800

    update push.bat again

--- push.bat
+++ push.bat
@@ -1,3 +1,6 @@
-git push origin --tags
-git push githubzuzhi --tags
-git push gitee --tags
+git push origin master --tags
+git push githubzuzhi master --tags
+git push gitee master --tags
+git push origin dev
+git push githubzuzhi dev
+git push gitee dev
