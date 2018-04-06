251884e615582ea163cbc871929f81330836e113
creatorlxd <creatorlxd@outlook.com>
2018-04-06 19:50:00 +0800

    feat : add config file;add file;add guidfactory;add inputdevice

---
+++ Source/GameEngine/Common/ConfigFile.cpp
@@ -0,0 +1,450 @@
+﻿/*
+Copyright 2018 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "stdafx.h"
+#include "ConfigFile.h"
+
+int SpaceGameEngine::ConfigFileValue::AsInt()
+{
+	if (m_Type == ConfigFileValueType::Int)
+	{
+		return std::stoi(StringToStdString(m_Content));
+	}
+	else
+	{
+		THROWERROR("this value's type is not int");
+		return 0;
+	}
+}
+
+float SpaceGameEngine::ConfigFileValue::AsFloat()
+{
+	if (m_Type == ConfigFileValueType::Float)
+	{
+		return std::stof(StringToStdString(m_Content));
+	}
+	else
+	{
+		THROWERROR("this value's type is not float");
+		return 0.00f;
+	}
+}
+
+double SpaceGameEngine::ConfigFileValue::AsDouble()
+{
+	if (m_Type == ConfigFileValueType::Double)
+	{
+		return std::stod(StringToStdString(m_Content));
+	}
+	else
+	{
+		THROWERROR("this value's type is not double");
+		return 0.00;
+	}
+}
+
+char SpaceGameEngine::ConfigFileValue::AsChar()
+{
+	if (m_Type == ConfigFileValueType::Char)
+	{
+		return m_Content[0];
+	}
+	else
+	{
+		THROWERROR("this value's type is not char");
+		return ' ';
+	}
+}
+
+SpaceGameEngine::String SpaceGameEngine::ConfigFileValue::AsString()
+{
+	if (m_Type == ConfigFileValueType::String)
+	{
+		return m_Content;
+	}
+	else
+	{
+		THROWERROR("this value's type is not string");
+		return "";
+	}
+}
+
+void SpaceGameEngine::ConfigFileValue::Set(int i)
+{
+	if (m_Type == ConfigFileValueType::Int)
+	{
+		m_Content = std::to_string(i);
+	}
+	else
+	{
+		THROWERROR("this value's type is not int");
+	}
+}
+
+void SpaceGameEngine::ConfigFileValue::Set(float f)
+{
+	if (m_Type == ConfigFileValueType::Float)
+	{
+		m_Content = std::to_string(f);
+	}
+	else
+	{
+		THROWERROR("this value's type is not float");
+	}
+}
+
+void SpaceGameEngine::ConfigFileValue::Set(double d)
+{
+	if (m_Type == ConfigFileValueType::Double)
+	{
+		m_Content = std::to_string(d);
+	}
+	else
+	{
+		THROWERROR("this value's type is not double");
+	}
+}
+
+void SpaceGameEngine::ConfigFileValue::Set(char c)
+{
+	if (m_Type == ConfigFileValueType::Char)
+	{
+		m_Content = c;
+	}
+	else
+	{
+		THROWERROR("this value's type is not char");
+	}
+}
+
+void SpaceGameEngine::ConfigFileValue::Set(const String & str)
+{
+	if (m_Type == ConfigFileValueType::String)
+	{
+		m_Content = str;
+	}
+	else
+	{
+		THROWERROR("this value's type is not string");
+	}
+}
+
+SpaceGameEngine::ConfigFileValue & SpaceGameEngine::ConfigTable::GetConfigValue(const String& name)
+{
+	auto iter = m_Content.find(name);
+	if (iter != m_Content.end())
+	{
+		return iter->second;
+	}
+	else
+	{
+		THROWERROR("can not find this config value");
+		return ConfigFileValue();
+	}
+}
+
+bool SpaceGameEngine::ConfigTable::IfHaveConfigValue(const String & name)
+{
+	auto iter = m_Content.find(name);
+	if (iter != m_Content.end())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+SpaceGameEngine::ConfigFile::ConfigFile()
+{
+
+}
+
+SpaceGameEngine::ConfigFile::ConfigFile(const String & filename)
+{
+	InitFromFile(filename);
+}
+
+SpaceGameEngine::ConfigTable & SpaceGameEngine::ConfigFile::GetConfigTable(const String & name)
+{
+	auto iter = m_Content.find(name);
+	if (iter != m_Content.end())
+	{
+		return iter->second;
+	}
+	else
+	{
+		THROWERROR("can not find this config table");
+		return ConfigTable();
+	}
+}
+
+bool SpaceGameEngine::ConfigFile::IfHaveConfigTable(const String & name)
+{
+	auto iter = m_Content.find(name);
+	if (iter != m_Content.end())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void SpaceGameEngine::ConfigFile::Parse(const Vector<String>& strs)
+{
+	/*
+	prase state:
+	0 : table
+	1 : key
+	2 : value
+	*/
+	int state = 0;
+	bool if_table_name = false;
+	ConfigTable* ptable = nullptr;
+	ConfigFileValue* pvalue = nullptr;
+	ConfigFileValueType type_buffer = ConfigFileValueType::Int;
+	String str_buffer;
+
+	for (auto str : strs)
+	{
+		for (auto _char : str)
+		{
+			if (_char == '\n' || _char == '\r' || _char == '\r\n')
+				continue;
+
+			//comment
+			if (_char == ';')
+				break;
+
+			//table name
+			if (state <= 1)
+			{
+				if (_char == '[' && (!if_table_name))
+				{
+					if_table_name = true;
+					continue;
+				}
+				if (_char == ']'&&if_table_name)
+				{
+					if (!str_buffer.empty())
+					{
+						state = 1;
+						m_Content.insert(std::make_pair(str_buffer, ConfigTable()));
+						ptable = &m_Content[str_buffer];
+						if_table_name = false;
+						str_buffer.clear();
+						continue;
+					}
+					else
+					{
+						THROWERROR("can not have null table name");
+						return;
+					}
+				}
+			}
+
+			if (if_table_name)
+			{
+				str_buffer += _char;
+				continue;
+			}
+
+			//key
+			if (state == 1)
+			{
+				if (_char == '=')
+				{
+					if (str_buffer.empty())
+					{
+						THROWERROR("config value need key name");
+						return;
+					}
+					else
+					{
+						if (ptable)
+						{
+							ptable->m_Content.insert(std::make_pair(str_buffer, ConfigFileValue()));
+							pvalue = &ptable->m_Content[str_buffer];
+							state = 2;
+							str_buffer.clear();
+							continue;
+						}
+						else
+						{
+							THROWERROR("can not find config table");
+							return;
+						}
+					}
+				}
+				else
+				{
+					if (_char != ' ')
+						str_buffer += _char;
+					continue;
+				}
+			}
+
+			//value
+			if (state == 2)
+			{
+				//float
+				if (_char == 'f'&& type_buffer == ConfigFileValueType::Double)
+				{
+					type_buffer = ConfigFileValueType::Float;
+					break;
+				}
+
+				//double
+				if (_char == '.'&& type_buffer == ConfigFileValueType::Int)
+				{
+					type_buffer = ConfigFileValueType::Double;
+				}
+
+				//char
+				if (_char == '\''&& type_buffer == ConfigFileValueType::Int)
+				{
+					type_buffer = ConfigFileValueType::Char;
+					continue;
+				}
+				if (_char == '\''&& type_buffer == ConfigFileValueType::Char)
+				{
+					if (str_buffer.size() > 1)
+					{
+						THROWERROR("char type value can only have single char");
+						return;
+					}
+					break;
+				}
+
+				//string
+				if (_char == '\"' && type_buffer == ConfigFileValueType::Int)
+				{
+					type_buffer = ConfigFileValueType::String;
+					continue;
+				}
+				if (_char == '\"' && type_buffer == ConfigFileValueType::String)
+				{
+					break;
+				}
+
+				if (type_buffer == ConfigFileValueType::Char || type_buffer == ConfigFileValueType::String)
+					str_buffer += _char;
+				else if (_char != ' ')
+					str_buffer += _char;
+			}
+		}
+
+		if (state == 2)
+		{
+			//string check
+			if (str_buffer.empty() && type_buffer != ConfigFileValueType::String)
+			{
+				THROWERROR("config value can not be null");
+				return;
+			}
+
+			//number check
+			if (type_buffer != ConfigFileValueType::Char&&type_buffer != ConfigFileValueType::String)
+			{
+				bool if_point = false;
+				for (auto iter = str_buffer.begin(); iter != str_buffer.end(); iter += 1)
+				{
+					auto i = *iter;
+					if (i == '.')
+					{
+						if (if_point || (type_buffer != ConfigFileValueType::Float && type_buffer != ConfigFileValueType::Double) || iter == str_buffer.end() - 1)
+						{
+							THROWERROR("config value number error");
+							return;
+						}
+						else
+							if_point = true;
+						continue;
+					}
+					if (i<'0' || i>'9')
+					{
+						THROWERROR("config value number error");
+						return;
+					}
+				}
+			}
+
+			//submit
+			if (pvalue)
+			{
+				pvalue->m_Type = type_buffer;
+				pvalue->m_Content = str_buffer;
+
+				type_buffer = ConfigFileValueType::Int;
+				str_buffer.clear();
+				state = 1;
+			}
+			else
+			{
+				THROWERROR("can not find config value");
+				return;
+			}
+		}
+	}
+}
+
+void SpaceGameEngine::ConfigFile::InitFromFile(const String & filename)
+{
+	Vector<String> buffer;
+	File file(filename, FileMode::Read);
+	String str_buff;
+
+	while (file.GetLine(str_buff))
+	{
+		buffer.push_back(str_buff);
+	}
+
+	file.Close();
+
+	Parse(buffer);
+}
+
+void SpaceGameEngine::ConfigFile::SaveToFile(const String & filename)
+{
+	File file(filename, FileMode::Write);
+
+	for (auto table : m_Content)
+	{
+		file << "[" << table.first << "]" << PrintMode::EndLine;
+		for (auto key : table.second.m_Content)
+		{
+			auto str = key.second.m_Content;
+			if (key.second.m_Type == ConfigFileValueType::Char)
+				str = "\'" + str + "\'";
+			if (key.second.m_Type == ConfigFileValueType::String)
+				str = "\"" + str + "\"";
+			if (key.second.m_Type == ConfigFileValueType::Float && (*str.rbegin()) != 'f')
+				str += 'f';
+			file << key.first << "=" << str << PrintMode::EndLine;
+		}
+
+	}
+
+	file.Close();
+}
+
+SpaceGameEngine::ConfigFile & SpaceGameEngine::GetDefaultConfigFile()
+{
+	static GlobalVariable<ConfigFile> DefaultConfigFile("SystemConfig.configfile");
+	return DefaultConfigFile.Get();
+}
---
+++ Source/GameEngine/Common/ConfigFile.h
@@ -0,0 +1,88 @@
+﻿/*
+Copyright 2018 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include "File.h"
+#include "GlobalVariable.h"
+#include "StringConverter.h"
+#include "Error.h"
+
+namespace SpaceGameEngine
+{
+	enum class ConfigFileValueType :unsigned char
+	{
+		Int,
+		Float,
+		Double,
+		Char,
+		String
+	};
+
+	struct ConfigFileValue
+	{
+		ConfigFileValueType m_Type;
+		String m_Content;
+
+		int AsInt();
+		float AsFloat();
+		double AsDouble();
+		char AsChar();
+		String AsString();
+
+		void Set(int i);
+		void Set(float f);
+		void Set(double d);
+		void Set(char c);
+		void Set(const String& str);
+	};
+
+	class ConfigTable
+	{
+	public:
+		friend class ConfigFile;
+
+		ConfigFileValue& GetConfigValue(const String& name);
+		bool IfHaveConfigValue(const String& name);
+	private:
+		Map<String, ConfigFileValue> m_Content;
+	};
+
+	class ConfigFile
+	{
+	public:
+		ConfigFile();
+		ConfigFile(const String& filename);
+
+		ConfigTable & GetConfigTable(const String& name);
+		bool IfHaveConfigTable(const String& name);
+
+		/*
+		接受由getline获取的一组字符串进行解析
+		*/
+		void Parse(const Vector<String>& strs);
+		void InitFromFile(const String& filename);
+		/*
+		保存配置表到某个文件，但不保留注释
+		*/
+		void SaveToFile(const String& filename);
+	public:
+		friend struct StdAllocatorInterface;
+		friend struct MemoryManagerAllocatorInterface;
+	private:
+		Map<String, ConfigTable> m_Content;
+	};
+
+	ConfigFile& GetDefaultConfigFile();
+}
