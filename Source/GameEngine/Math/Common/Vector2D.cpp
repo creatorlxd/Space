8c1fdeffcc6fddfc9c615f1ff33a214f65879d10
creatorlxd <creatorlxd@outlook.com>
2018-06-01 23:24:37 +0800

    add math vector2d

---
+++ Source/GameEngine/Math/Common/Vector2D.cpp
@@ -0,0 +1,62 @@
+﻿/*
+Copyright 2018 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "stdafx.h"
+#include "Vector2D.h"
+
+SpaceGameEngine::Vector2D::Vector2D(float x, float y)
+	:XMFLOAT2(x,y)
+{
+}
+
+SpaceGameEngine::Vector2D::Vector2D(const XMFLOAT2 & c)
+	:XMFLOAT2(c)
+{
+}
+
+SpaceGameEngine::Vector2D::Vector2D(const Vector2D & v)
+	:XMFLOAT2(v.x,v.y)
+{
+}
+
+SpaceGameEngine::Vector2D::Vector2D(FXMVECTOR v)
+{
+	XMStoreFloat2(this, v);
+}
+
+bool SpaceGameEngine::Vector2D::operator=(const XMFLOAT2 & c)
+{
+	x = c.x;
+	y = c.y;
+	return true;
+}
+
+bool SpaceGameEngine::Vector2D::operator=(const Vector2D & v)
+{
+	x = v.x;
+	y = v.y;
+	return true;
+}
+
+bool SpaceGameEngine::Vector2D::operator=(FXMVECTOR v)
+{
+	XMStoreFloat2(this, v);
+	return true;
+}
+
+SpaceGameEngine::Vector2D::operator XMVECTOR()
+{
+	return XMLoadFloat2(this);
+}
---
+++ Source/GameEngine/Math/Common/Vector2D.h
@@ -0,0 +1,34 @@
+﻿/*
+Copyright 2018 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include "Def.h"
+
+namespace SpaceGameEngine
+{
+	struct Vector2D :public XMFLOAT2
+	{
+		Vector2D(float x, float y);
+		Vector2D(const XMFLOAT2& c);
+		Vector2D(const Vector2D& v);
+		Vector2D(FXMVECTOR v);
+
+		bool operator = (const XMFLOAT2& c);
+		bool operator = (const Vector2D& v);
+		bool operator = (FXMVECTOR v);
+
+		operator XMVECTOR();
+	};
+}
