cf4231f82a7b21215f98ab8d5edd53d69cb1c7a2
creatorlxd <1461637345@qq.com>
2018-08-31 13:18:08 +0800

    fix

--- test.ps1
+++ test.ps1
@@ -1,20 +1,22 @@
 # use for appveyor test
 if ($CONFIGURATION -eq "Debug")
 {
+	Write-Output("debug")
 	exit 0
 }
 if ($CONFIGURATION -eq "Release")
 {
+	Write-Output("release")
 	exit 0
 }
 if ($PLATFORM -eq "x64") {
-	cmd /c copy .\x64\%CONFIGURATION%\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe 
-	cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
+	cmd /c copy %APPVEYOR_BUILD_FOLDER%\x64\%CONFIGURATION%\UnitTest.exe %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe 
+	cmd /c %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
 	exit $LASTEXITCODE
 }
 else 
 {
-	cmd /c copy .\%CONFIGURATION%\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe
-	cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
+	cmd /c copy %APPVEYOR_BUILD_FOLDER%\%CONFIGURATION%\UnitTest.exe .%APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe
+	cmd /c %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
 	exit $LASTEXITCODE
 }
