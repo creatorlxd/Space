fb37dfadf3494bfd86e8c254f62449e6edd35eb0
creatorlxd <1461637345@qq.com>
2018-08-31 14:12:51 +0800

    remove unuseful script

--- run_unit_test.bat
+++
@@ -1,4 +0,0 @@
-copy .\BuildDebug\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTestx86debug.exe && .\Source\UnitTest\UnitTest\UnitTestx86debug.exe -all
-copy .\BuildRelease\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTestx86release.exe && .\Source\UnitTest\UnitTest\UnitTestx86release.exe -all
-copy .\x64\BuildDebug\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTestx64debug.exe && .\Source\UnitTest\UnitTest\UnitTestx64debug.exe -all
-copy .\x64\BuildRelease\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTestx64release.exe && .\Source\UnitTest\UnitTest\UnitTestx64release.exe -all
