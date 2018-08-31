a0915f113b6e84df87f33b5361ce95f5ec851870
creatorlxd <1461637345@qq.com>
2018-08-31 13:39:10 +0800

    fix

--- test.ps1
+++ test.ps1
@@ -1,15 +1,13 @@
 # use for appveyor test
-if ($CONFIGURATION -eq "Debug")
+if ($env:CONFIGURATION -eq "Debug")
 {
-	Write-Output("debug")
 	exit 0
 }
-if ($CONFIGURATION -eq "Release")
+if ($env:CONFIGURATION -eq "Release")
 {
-	Write-Output("release")
 	exit 0
 }
-if ($PLATFORM -eq "x64") {
+if ($env:PLATFORM -eq "x64") {
 	cmd /c copy %APPVEYOR_BUILD_FOLDER%\x64\%CONFIGURATION%\UnitTest.exe %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe 
 	cmd /c %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
 	exit $LASTEXITCODE
