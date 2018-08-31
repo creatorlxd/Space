7a9f87ec047825bfa9ff26a9a2b7e64d7f2594df
creatorlxd <1461637345@qq.com>
2018-08-31 12:21:16 +0800

    fix test shell

--- test.ps1
+++ test.ps1
@@ -1,8 +1,33 @@
+if ($CONFIGURATION -eq "Debug")
+{
+	exit 0
+}
+if ($CONFIGURATION -eq "Release")
+{
+	exit 0
+}
 if ($PLATFORM -eq "x64") {
 	cmd /c copy .\x64\%CONFIGURATION%\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%x64.exe 
-	cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%x64.exe -all
+	$result = cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%x64.exe -all
+	if($result.HasSuccessed)
+	{
+		exit 0
+	}
+	else
+	{
+		exit -1	
+	}
 }
-else {
+else 
+{
 	cmd /c copy .\%CONFIGURATION%\UnitTest.exe .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe
-	cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
+	$result = cmd /c .\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
+	if($result.HasSuccessed)
+	{
+		exit 0
+	}
+	else
+	{
+		exit -1	
+	}
 }
