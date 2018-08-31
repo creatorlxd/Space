70d0026cb6abd67a1b8c5b0d0462da1505261a01
creatorlxd <1461637345@qq.com>
2018-08-31 13:28:52 +0800

    fix ci

--- test.ps1
+++ test.ps1
@@ -10,15 +10,13 @@ if ($CONFIGURATION -eq "Release")
 	exit 0
 }
 if ($PLATFORM -eq "x64") {
-	Write-Output(%APPVEYOR_BUILD_FOLDER%\x64\%CONFIGURATION%\UnitTest.exe)
 	cmd /c copy %APPVEYOR_BUILD_FOLDER%\x64\%CONFIGURATION%\UnitTest.exe %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe 
 	cmd /c %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
 	exit $LASTEXITCODE
 }
 else 
 {
-	Write-Output(%APPVEYOR_BUILD_FOLDER%\%CONFIGURATION%\UnitTest.exe)
-	cmd /c copy %APPVEYOR_BUILD_FOLDER%\%CONFIGURATION%\UnitTest.exe .%APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe
+	cmd /c copy %APPVEYOR_BUILD_FOLDER%\%CONFIGURATION%\UnitTest.exe %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe
 	cmd /c %APPVEYOR_BUILD_FOLDER%\Source\UnitTest\UnitTest\UnitTest%CONFIGURATION%.exe -all
 	exit $LASTEXITCODE
 }
