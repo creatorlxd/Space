bd9ae704d51025beb212d5e75e9b73a3b9746519
creatorlxd <creatorlxd@outlook.com>
2018-10-20 23:09:30 +0800

    add comment for config file .h

--- Source/GameEngine/Common/ConfigFile.h
+++ Source/GameEngine/Common/ConfigFile.h
@@ -21,6 +21,9 @@ limitations under the License.
 
 namespace SpaceGameEngine
 {
+	/*!
+	@brief 配置文件键值的类型
+	*/
 	enum class ConfigFileValueType :unsigned char
 	{
 		Int,
@@ -31,23 +34,106 @@ namespace SpaceGameEngine
 		Bool
 	};
 
+	/*!
+	@brief 配置文件中的值
+	@note 实际上是使用String来储存和转换
+	*/
 	struct ConfigFileValue
 	{
+		/*!
+		@brief 值的类型
+		@see ConfigFileValueType
+		*/
 		ConfigFileValueType m_Type;
+		/*!
+		@brief 值的内容
+		*/
 		String m_Content;
 
+		/*!
+		@brief 以`int`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Int`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		int AsInt();
+		/*!
+		@brief 以`float`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Float`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		float AsFloat();
+		/*!
+		@brief 以`double`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Double`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		double AsDouble();
+		/*!
+		@brief 以`char`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Char`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		char AsChar();
+		/*!
+		@brief 以`SpaceGameEngine::String`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::String`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		String AsString();
+		/*!
+		@brief 以`bool`类型得到值的内容
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Bool`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		bool AsBool();
 
+		/*!
+		@brief 设置值为一个`int`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Int`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(int i);
+		/*!
+		@brief 设置值为一个`float`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Float`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(float f);
+		/*!
+		@brief 设置值为一个`double`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Double`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(double d);
+		/*!
+		@brief 设置值为一个`char`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Char`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(char c);
+		/*!
+		@brief 设置值为一个`SpaceGameEngine::String`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::String`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(const String& str);
+		/*!
+		@brief 设置值为一个`bool`值
+		@attention 值的类型`m_Type`应该是`ConfigFileValueType::Bool`
+		@see m_Type
+		@see ConfigFileValueType
+		*/
 		void Set(bool b);
 	};
 
